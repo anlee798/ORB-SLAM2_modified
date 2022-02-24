ffa8d6f4fcf61d6253d5cf232ef0d8b96b68efc9
zhuanlei <2578329547@qq.com>
2022-02-24 15:38:24 +0800

    first commit

---
+++ Examples/ROS/ORB_SLAM2/build/gtest/googlemock/CMakeFiles/gmock.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gmock.dir/__/googletest/src/gtest-all.cc.o"
+  "CMakeFiles/gmock.dir/src/gmock-all.cc.o"
+  "/home/anlei/ORB_SLAM2_modified/Examples/ROS/ORB_SLAM2/lib/libgmock.pdb"
+  "/home/anlei/ORB_SLAM2_modified/Examples/ROS/ORB_SLAM2/lib/libgmock.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gmock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ g2o_with_orbslam2/CMakeFiles/rgbd_my.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rgbd_my.dir/Examples/RGB-D/rgbd_my.cc.o"
+  "../Examples/RGB-D/rgbd_my.pdb"
+  "../Examples/RGB-D/rgbd_my"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rgbd_my.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
